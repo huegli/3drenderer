a6832bb9580a3f8b6443efe32e2056a13a0830c6
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2024-05-03 08:31:34 -0700

    VS 2022 Solution/Project setup

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <SDL.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING); // Initialize SDL
+
     printf("Hello, world!\n");
     return 0;
 }
