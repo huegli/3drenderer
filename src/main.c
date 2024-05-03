241909531f9e4cff20da3f4678eeff9836a08924
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2024-05-03 16:48:11 -0700

    Create Window, Renderer and color buffer

--- src/main.c
+++ src/main.c
@@ -1,10 +1,100 @@
+
+#include "SDL_render.h"
+#include "SDL_stdinc.h"
+#include "SDL_video.h"
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <SDL.h>
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING); // Initialize SDL
-    SDL_Init(SDL_INIT_EVERYTHING);
+bool is_running = false;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
-    printf("Hello, world!\n");
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+
+    // Create a SDL Window
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        window_width,
+        window_height,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        fprintf(stderr, "Error creating SDL Window.\n");
+        return false;
+    }
+
+    // Create a SDL Renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void) {
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                is_running = false;
+            break;
+    }
+
+}
+
+void update(void) {
+    // TODO
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+
+    is_running = initialize_window();
+
+    setup();
+
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
+
+    destroy_window();
+    
     return 0;
 }
