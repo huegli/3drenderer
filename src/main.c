65d2d55fd7f6dd9d1802458fe8d497ed4458af1e
Nikolai Schlegel <nschlegel@apple.com>
2024-05-14 10:20:13 -0700

    Dynamic array for projected triangles

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
+#include "array.h"
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
-
-triangle_t triangles_to_render[N_MESH_FACES];
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = 5 };
 vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
@@ -12,6 +11,8 @@ float fov_factor = 640;
 bool is_running = false;
 
 int previous_frame_time = 0;
+
+triangle_t* triangles_to_render = NULL;
 
 void setup(void)
 {
@@ -66,6 +67,8 @@ void update(void)
 
     previous_frame_time = SDL_GetTicks();
 
+    triangles_to_render = NULL;
+    
     cube_rotation.x += 0.01;
     cube_rotation.y += 0.01;
     cube_rotation.z += 0.01;
@@ -107,10 +110,8 @@ void update(void)
             projected_triangle.points[j] = projected_point;
         }
 
-
-
-       // Save the projected triangle in the array of triangles to render
-        triangles_to_render[i] = projected_triangle;
+        // Save the projected triangle in the array of triangles to render
+        array_push(triangles_to_render, projected_triangle);
 
     }
 }
@@ -119,7 +120,8 @@ void render(void)
 {
     draw_grid();
 
-    for (int i = 0; i < N_MESH_FACES; i++) {
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
         
         draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
@@ -133,8 +135,11 @@ void render(void)
     
 
     }
+    
+    array_free(triangles_to_render);
 
     render_color_buffer();
+    
     clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
