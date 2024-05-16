5a592d4af9133febdaaf6806b7b6589788fb3831
Nikolai Schlegel <nschlegel@apple.com>
2024-05-15 20:25:29 -0700

    Reading in Obj files

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ void setup(void)
         SDL_TEXTUREACCESS_STREAMING,
         window_width, window_height);
 
-    load_cube_mesh_data();
+    load_obj_file_data("./assets/f22.obj");
 }
 
 void process_input(void)
--- src/mesh.c
+++ src/mesh.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "array.h"
 #include "mesh.h"
 
@@ -50,4 +51,81 @@ void load_cube_mesh_data(void)
         face_t cube_face = cube_faces[i];
         array_push(mesh.faces, cube_face);
     }
+}
+
+void load_obj_file_data(char *filename) {
+    FILE *file;
+    char line[100];
+
+    // Open the file
+    file = fopen(filename, "r");
+
+    // Check if file opened successfully
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    // Read each line until end of file is reached
+    while (fgets(line, sizeof(line), file) != NULL) {
+
+        char *token = strtok(line, " ");
+        if (token != NULL) {
+            
+            if (strcmp(token, "v") == 0) {
+                float x, y, z;
+                
+                token = strtok(NULL, " ");
+                if (token == NULL) return;
+                if (sscanf(token, "%f", &x) != 1) return;
+                
+                token = strtok(NULL, " ");
+                if (token == NULL) return;
+                if (sscanf(token, "%f", &y) != 1) return;
+                
+                token = strtok(NULL, " ");
+                if (token == NULL) return;
+                if (sscanf(token, "%f", &z) != 1) return;
+                
+                vec3_t v = {
+                    x = x,
+                    y = y,
+                    z = z
+                };
+                
+                array_push(mesh.vertices, v);
+                
+                // printf("(%f, %f ,%f)\n", x, y, z);
+            }
+            
+            if (strcmp(token, "f") == 0) {
+                int a, b, c;
+                int dummy1, dummy2;
+                
+                token = strtok(NULL, " ");
+                if (token == NULL) return;
+                if (sscanf(token, "%d/%d/%d", &a, &dummy1, &dummy2) != 3) return;
+                
+                token = strtok(NULL, " ");
+                if (token == NULL) return;
+                if (sscanf(token, "%d/%d/%d", &b, &dummy1, &dummy2) != 3) return;
+                
+                token = strtok(NULL, " ");
+                if (token == NULL) return;
+                if (sscanf(token, "%d/%d/%d", &c, &dummy1, &dummy2) != 3) return;
+                
+                face_t f = {
+                    a = a,
+                    b = b,
+                    c = c
+                };
+                
+                array_push(mesh.faces, f);
+                // printf("(%d, %d ,%d)\n", f1, f2, f3);
+            }
+        }
+    }
+
+    // Close the file
+    fclose(file);
 }
