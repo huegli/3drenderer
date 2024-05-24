46432547544f0811fa6e6a662d71794522598d1e
Nikolai Schlegel <nschlegel@apple.com>
2024-05-24 15:19:31 -0700

    Flat-top & Flat-bottom triangle fill

--- src/main.c
+++ src/main.c
@@ -160,12 +160,15 @@ void render(void)
         draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
         draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
         
+        draw_filled_triangle(
+            triangle.points[0].x, triangle.points[0].y,
+            triangle.points[1].x, triangle.points[1].y,
+            triangle.points[2].x, triangle.points[2].y, 0xFFFFFFFF);
         draw_triangle(
             triangle.points[0].x, triangle.points[0].y,
             triangle.points[1].x, triangle.points[1].y,
-            triangle.points[2].x, triangle.points[2].y, 0xFF00FF00);
+            triangle.points[2].x, triangle.points[2].y, 0xFF000000);
     
-
     }
     
     array_free(triangles_to_render);
