5aee84222efa705fbb059231d79e1f84b7e86421
Nikolai Schlegel <nschlegel@apple.com>
2024-07-04 14:32:58 -0700

    Rotation added

--- src/main.c
+++ src/main.c
@@ -125,7 +125,10 @@ void update(void)
     // create a scale matrix that will be used to multiply the mesh vertices
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
     mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z );
-    
+    mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+    mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+    mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
+
     // loop through all triangle faces of our mesh
     int num_faces = array_length(mesh.faces);
     for (int i = 0; i < num_faces; i++) {
@@ -143,18 +146,10 @@ void update(void)
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
             transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
             transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
-            
-//            // Rotate the point around the X axis
-//            transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
-//
-//            // Rotate the point around the Y axis
-//            transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
-//
-//            // Rotate the point around the Z axis
-//            transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
-
-
 
             // Save the transformed vertex in the array of transformed vertices
             transformed_vertices[j] = transformed_vertex;
